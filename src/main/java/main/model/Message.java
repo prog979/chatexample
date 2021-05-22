032ee09b45a714cc3b99106a4cbd61fd8b527a42
Oleg <prog979@yandex.ru>
2021-05-22 16:58:56 +0300

    added message.java in package model

---
+++ src/main/java/main/model/Message.java
@@ -0,0 +1,22 @@
+package main.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int messageId;
+    private Date mesageTimeStump;
+    private int userId;
+    private String messageText ;
+}
